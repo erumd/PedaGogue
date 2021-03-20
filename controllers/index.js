29b3567e30e667ca412a5cb77b7225530b3fb627
Jas James <jasdjames@gmail.com>
2021-03-20 12:22:14 -0500

    changed get routes

--- controllers/index.js
+++ controllers/index.js
@@ -2,14 +2,14 @@ const router = require('express').Router();
 // const { Post } = require('../models');
 // const { User } = require('../models');
 
-const apiRoutes = require('./api');
-const userRoutes = require('./api./userRoutes.js');
+// const apiRoutes = require('./api');
+const userRoutes = require('./api/userRoutes.js');
 
-router.use('/', apiRoutes);
+// router.use('/', apiRoutes);
 router.use('/', userRoutes);
 
-router.get('/', (req, res) =>
-  res.send('Pedagogue Under Construction. Trying to set up Routes ')
-);
+// router.get('/', (req, res) =>
+//   res.send('Pedagogue Under Construction. Trying to set up Routes ')
+// );
 
 module.exports = router;
