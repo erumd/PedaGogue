cef4f968d4c00a75e8f2f89dcefd92137c4bc5f4
Mr. Llanes <llanesm@gmail.com>
2021-03-24 19:45:28 -0500

    Small changes to the handlebars and scripts.

--- public/js/login.js
+++ public/js/login.js
@@ -7,7 +7,7 @@ const loginFormHandler = async (event) => {
 
   if (username && password) {
     // Send a POST request to the API endpoint
-    const response = await fetch('api/users', {
+    const response = await fetch('users/login', {
       method: 'POST',
       body: JSON.stringify({ username, password }),
       headers: { 'Content-Type': 'application/json' },
@@ -15,7 +15,7 @@ const loginFormHandler = async (event) => {
 
     if (response.ok) {
       // If successful, redirect the browser to the profile page
-      document.location.replace('/profile');
+      document.location.replace('/topiclist');
     } else {
       alert(response.statusText);
       
