cdeb4770b4f93eed47aac3e1390090d70beb4e05
Jas James <jasdjames@gmail.com>
2021-03-24 19:07:00 -0500

    flogin route

--- public/js/login.js
+++ public/js/login.js
@@ -7,7 +7,7 @@ const loginFormHandler = async (event) => {
 
   if (username && password) {
     // Send a POST request to the API endpoint
-    const response = await fetch('api/users', {
+    const response = await fetch('/users/login', {
       method: 'POST',
       body: JSON.stringify({ username, password }),
       headers: { 'Content-Type': 'application/json' },
@@ -15,10 +15,9 @@ const loginFormHandler = async (event) => {
 
     if (response.ok) {
       // If successful, redirect the browser to the profile page
-      document.location.replace('/profile');
+      document.location.replace('/topicList');
     } else {
       alert(response.statusText);
-      
     }
   }
 };
