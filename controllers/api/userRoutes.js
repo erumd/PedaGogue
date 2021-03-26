2858efb8f36366dbbb89752d3ecb24ad61c83be4
Mr. Llanes <llanesm@gmail.com>
2021-03-25 20:29:51 -0500

    Button login route issue fixed.

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -123,7 +123,9 @@ router.get('/login', (req, res) => {
 router.get('/topicList', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.render('topicList');
+    res.render('topicList', {
+      logged_in: true,
+    });
     return;
   }
   res.redirect('/login');
