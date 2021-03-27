e6335e741cf3985b2319a0a2bf3e94e1db7a1472
Erum Dhukka <erumdhukka531@gmail.com>
2021-03-26 23:18:57 -0500

    refactored delete button to topic.js

--- public/js/topic.js
+++ public/js/topic.js
@@ -1,19 +1,19 @@
-// const delButtonHandler = async (event) => {
-//     if (event.target.hasAttribute('name')) {
-//       const id = event.target.getAttribute('name');
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute('name')) {
+    const id = event.target.getAttribute('name');
 
-//       const response = await fetch(`/user${id}`, {
-//         method: 'DELETE',
-//       });
+    const response = await fetch(`/user${id}`, {
+      method: 'DELETE',
+    });
 
-//       if (response.ok) {
-//         document.location.replace('/topic');
-//       } else {
-//         alert('Failed to delete project');
-//       }
-//     }
-//   };
+    if (response.ok) {
+      document.location.replace('/topic');
+    } else {
+      alert('Failed to delete project');
+    }
+  }
+};
 
-// document
-// .querySelector('.project-list')
-// .addEventListener('click', delButtonHandler);
+document
+  .querySelector('.project-list')
+  .addEventListener('click', delButtonHandler);
